f804cbb416917df723814f6fc1b9726d7f09d57a
Sourena Khanzadeh <sourena.khanzadeh@ryerson.ca>
2022-11-14 16:30:49 -0500

    Added: SDL as a submodule

--- src/experiments/vector_test.cpp
+++ src/experiments/vector_test.cpp
@@ -1,13 +1,29 @@
 #include "../../include/almat.h"
+#include <SDL.h>
 
 int main(){
-    ALMat<float> A(3, 3, 1.0);
-    std::cout << A << std::endl;
-    A.identity();
-    
+    // make a window
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window* window = SDL_CreateWindow("Vector Test", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_SHOWN);
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
 
-    std::cout << A << std::endl;
-    
+    // update the window
+    bool running = true;
+    while(running){
+        SDL_Event event;
+        while(SDL_PollEvent(&event)){
+            if(event.type == SDL_QUIT){
+                running = false;
+            }
+        }
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+        SDL_RenderPresent(renderer);
+    }
 
     return 0;
 }
