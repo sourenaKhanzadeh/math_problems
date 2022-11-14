4a7f92a607c9fdccb87657b3c6dfbac2ca24a771
Sourena Khanzadeh <sourena.khanzadeh@ryerson.ca>
2022-11-14 14:54:15 -0500

    Updated .gitignore

--- build/CMakeFiles/math_problems.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for math_problems.
-# This may be replaced when dependencies are built.
--- src/experiments/vector_test.cpp
+++ src/experiments/vector_test.cpp
@@ -3,15 +3,7 @@
 int main(){
     ALMat<float> A(3, 3, 1.0);
     std::cout << A << std::endl;
-    A[0][0] = 1;
-    A[0][1] = 2;
-    A[0][2] = 3;
-    A[1][0] = 4;
-    A[1][1] = 5;
-    A[1][2] = 6;
-    A[2][0] = 7;
-    A[2][1] = 8;
-    A[2][2] = 9;
+    A.identity();
     
 
     std::cout << A << std::endl;
