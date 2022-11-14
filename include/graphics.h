fa21d2cff8a841a922a11a0997ea1790c8998185
Sourena Khanzadeh <sourena.khanzadeh@ryerson.ca>
2022-11-14 17:35:56 -0500

    Added: base graph
    Base catesion graphs

--- include/graphics.h
+++ include/graphics.h
@@ -2,6 +2,29 @@
 #define GRAPHICS_H
 
 #include <SDL.h>
+#include <vector>
+
+namespace gp{
+    class GameObject{
+    public:
+        virtual void update() = 0;
+        virtual void draw() = 0;
+    };
+}
+
+namespace gp{
+    class Graph : public GameObject{
+    public:
+        Graph(SDL_Window* window, SDL_Renderer* renderer);
+        ~Graph();
+        void draw();
+        void update();
+    private:
+        SDL_Window* window;
+        int width, height;
+        SDL_Renderer* renderer;
+    };
+}
 
 namespace gp{
     class Window{
@@ -17,7 +40,9 @@ namespace gp{
         SDL_Window* window;
         SDL_Renderer* renderer;
         bool running;
+        Graph* graph;
     };
 }
+
 
 #endif
--- src/utils/graphics.cpp
+++ src/utils/graphics.cpp
@@ -6,6 +6,7 @@ namespace gp{
         window = SDL_CreateWindow(title, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width, height, SDL_WINDOW_SHOWN);
         renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
         running = true;
+        graph = new Graph(window, renderer);
     }
     Window::~Window(){
         SDL_DestroyRenderer(renderer);
@@ -19,16 +20,59 @@ namespace gp{
                 running = false;
             }
         }
+
+        graph->update();
     }
     void Window::clear(){
         SDL_SetRenderDrawColor(renderer, 0xff, 0xff, 0xff, 255);
         SDL_RenderClear(renderer);
     }
     void Window::draw(){
+
+        graph->draw();
+
         SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
         SDL_RenderPresent(renderer);
     }
     void Window::close(){
         running = false;
+    }
+}
+
+namespace gp{
+    Graph::Graph(SDL_Window* window, SDL_Renderer* renderer){
+        this->window = window;
+        this->renderer = renderer;
+        int w, h;
+        // get window height and width
+        SDL_GetWindowSize(window, &w, &h);
+        this->width = w;
+        this->height = h;
+    }
+    
+    Graph::~Graph(){
+    }
+    void Graph::draw(){
+        // draw the cartesian plane
+        // draw the x and y axis
+        // draw the x and y labels
+        // draw the x and y ticks
+        // draw the x and y tick labels
+        // draw the graph
+
+        // draw the x axis
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderDrawLine(renderer, 0, height/2, width, height/2);
+        // draw the y axis
+        SDL_RenderDrawLine(renderer, width/2, 0, width/2, height);
+
+        // draw the x ticks
+        for(int i = 0; i < width; i += 10){
+            SDL_RenderDrawLine(renderer, i, height/2 - 5, i, height/2 + 5);
+        }
+
+    }
+    void Graph::update(){
+
     }
 }
